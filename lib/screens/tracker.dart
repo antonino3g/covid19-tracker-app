0412c91a2c9f52a443be42758d313bdafb049abd
Antonino Praxedes <antonino.praxedes@gmail.com>
2021-11-28 23:00:51 -0400

    feat adding navigation option Global & Country

--- lib/screens/tracker.dart
+++ lib/screens/tracker.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import '../utils/constants.dart';
+import 'navigation_option.dart';
+
+enum NavigationStatus {
+  GLOBAL,
+  COUNTRY,
+}
 
 class Tracker extends StatefulWidget {
   const Tracker({Key? key}) : super(key: key);
@@ -9,16 +15,63 @@ class Tracker extends StatefulWidget {
 }
 
 class _TrackerState extends State<Tracker> {
+  NavigationStatus navigationStatus = NavigationStatus.GLOBAL;
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: kPrimaryColor,
         elevation: 0,
         title: Text(
-          "COVID-19",
+          'COVID-19',
         ),
         centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                color: kPrimaryColor,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                  bottomLeft: Radius.circular(50),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: size.height * 0.1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                NavigationOption(
+                  title: 'Mundo',
+                  selected: navigationStatus == NavigationStatus.GLOBAL,
+                  onSelected: () {
+                    setState(() {
+                      navigationStatus = NavigationStatus.GLOBAL;
+                    });
+                  },
+                ),
+                NavigationOption(
+                  title: 'País',
+                  selected: navigationStatus == NavigationStatus.COUNTRY,
+                  onSelected: () {
+                    setState(() {
+                      navigationStatus = NavigationStatus.COUNTRY;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
